01aa7f37ee559e127b1d17ada4db3fd6d12b3357
derekheidorn@sierrasystems.com <2-7#ws7PEZCfnIE#nD$#2w>
2018-12-11 21:32:50 -0800

    Need to change spring boot implementation

--- src/main/java/com/example/demo/ReportsApplication.java
+++ src/main/java/com/example/demo/ReportsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @SpringBootApplication
-public class ReportsApplication extends ResourceServerConfigurerAdapter {
+public class ReportsApplication extends ResourceServerConfigurerAdapter { // SpringBootServletInitializer
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportsApplication.class, args);
